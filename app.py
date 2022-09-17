8fb57d3d063762c598f02a8aef57b8669abe9616
Agbleze <lagbleze@st.ug.edu.gh>
2022-09-17 03:45:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,19 @@ from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 import joblib
 from sklearn.pipeline import Pipeline
-from typing import List, Any
+from typing import List, Any, Optional, Tuple
+import os
 
+#%%
+loaded_model = joblib.load("model_used.model")
+
+#%%
+model_path = os.path.join(os.path.dirname(__file__), "model_used.model")
+
+#%%
+joblib.load(model_path)
+
+#%%
 class PredictionInput(BaseModel):
     parameter: List[Any]
 
@@ -12,11 +23,84 @@ class PredictionOutput(BaseModel):
     emission: float
 
 
+class CarbonEmissionModel:
+    model: Optional[Pipeline]
+    
+    
+    def load_model(self):
+        """Load the model
+        """
+        model_file = os.path.join(os.path.dirname(__file__), "model_used.model")
+        loaded_model: Pipeline = joblib.load(model_file)
+        #model, targets = loaded_model
+        self.model = loaded_model
+        #self.targets = targets
+        
+    async def predict(self, input: PredictionInput) -> PredictionOutput:
+        """Runs a prediction
+
+        Args:
+            PredictionInput (_type_): _description_
+
+        Returns:
+            PredictionOutput: _description_
+        """
+        if not self.model:
+            raise RuntimeError("Model is not loaded")
+        prediction = self.model.predict([input.parameter])
+        
+        
+
+
+#%%
+def make_prediction(state_selected, lga_selected, sector_selected, credit_amt, 
+                    income_amt, predict_button):
+    ctx = callback_context
+    button_clicked = ctx.triggered[0]['prop_id'].split('.')[0]
+    
+    # prediction_input = [state_selected, lga_selected, sector_selected, credit_amt,
+    #                     income_amt]
+    
+    
+    
+    prediction_inputs = {'state_name': state_selected, 'lga': lga_selected, 
+                         'sector': sector_selected, 'credit_mean': credit_amt, 
+                         'income_mean': income_amt
+                         }
+    prediction_inputs_df = pd.DataFrame(data=prediction_inputs, index=[0])
+
+    
+    if ((not button_clicked) or (button_clicked != 'id_predict_emission') 
+        or (not any(prediction_inputs_df)) or (not predict_button)
+        ):
+        PreventUpdate
+        
+    if button_clicked == 'id_predict_emission':
+        
+        if not all(prediction_inputs_df):
+            PreventUpdate
+            
+            # message = ('All parameters must be provided. Either some values have not \
+            #             been provided or invalid values were provided. Please select the \
+            #            right values for all parameters from the dropdown. \
+            #             Then, click on predict clicks button to \
+            #             predict number of clicks'
+            #            )
+            # return True, message, dash.no_update
+        
+        if all(prediction_inputs_df):
+            result = loaded_model.predict(prediction_inputs_df)[0]
+            prediction = round(result)
+            prediction_desc = f'Household with the selected characteristics is predicted to emit {prediction} kg carbon dioxide'
+            return (prediction, prediction_desc) #False, dash.no_update, prediction
+        
+        
+        
+    #%%
+    loaded_model.predict(1, 2, 3, 4, 5)
 
 
 
 
 
-
-
-
+# %%

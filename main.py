0cc6d75b0a1e2068a86ee31d7e5c5fa3e0b08231
Agbleze <lagbleze@st.ug.edu.gh>
2022-09-19 00:43:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from typing import List, Any, Optional, Tuple
 import os
 import pandas as pd
 from carbon_emission import PredictionOutput, PredictionInput, CarbonEmissionModel
-
+import uvicorn
 # instantiate class
 carbon_emision_model = CarbonEmissionModel()
 
@@ -27,7 +27,8 @@ async def get_prediction(output: PredictionOutput = Depends(carbon_emision_model
 async def startup():
     carbon_emision_model.load_model()
 
-
+if __name__ == '__main__':
+    uvicorn.run(app=app, port=8000, log_level='info')
 #%%
 # echo '{"parameter": {"state_name": "Bayela", "lga": 108, "sector": "RURAL", "credit_mean": 70, "income_mean": 600}}' | http POST http://127.0.0.1:8000/predict
 
